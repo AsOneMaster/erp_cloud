5b07808441218358fa98553629ee79a83e5c9fcf
unknown <unknown@example.com>
2020-08-18 22:31:41 +0800

    Initial Commit

---
+++ miniprogram/pages/application/application.js
@@ -0,0 +1,21 @@
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  data: {
+    elements: [
+      {
+        title: '文件管理',
+        name: 'fileManagement',
+        color: 'olive',
+        icon: 'copy'
+      },
+      {
+        title: '经验库',
+        name: 'experienceBase',
+        color: 'green',
+        icon: 'loading'
+      },
+    ],
+  }
+})
